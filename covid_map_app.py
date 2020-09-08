d697264e33ec42b3b1dde8a519c4c25bc2185ab8
Sebastian Pokrywka <pokrywkasebastian1998@gmail.com>
2020-09-08 17:24:15 +0200

    add write data to csv file

--- covid_map_app.py
+++ covid_map_app.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import numpy as np
+import pandas as pd
 link=requests.get("https://www.worldometers.info/coronavirus/?fbclid=IwAR2cvtDz4w1I7jJn6y0tzl-rN8yxr6-dEvkEPQ8uZrxWPLPZVk3t0RSKRpQ")
 
 date=link.text
@@ -12,7 +13,7 @@ country_array=[]
 for country in soup.find_all("a",{"class":"mt_a"}):
     tmp_country=country.get_text()
     country_array.append(tmp_country)
-print(country_array)
+
 
 #download total cases
 totalCases_array=[]
@@ -21,16 +22,30 @@ tmp =soup.find_all("tr")
 
 for i in range(9,223):
     totalCases_array.append((tmp[i].find_all("td",{"style":"font-weight: bold; text-align:right"})))
-for i in range(0,213):
-    print(totalCases_array[i][0].text)
+#print(totalCases_array[1])
 
-#for i in range(0,2123):
-#    print(i+1," Panistwo ",country_array[i]," Liczba zarazen ", totalCases_array[i][0].text)
+#download total deaths
+totalDeaths_array=[]
+
+for i in range(8,223):
+    totalDeaths_array.append((tmp[i].find_all("td")))
+#print(totalDeaths_array[2][4])
+#for i in range(1,215):
+#    print(totalDeaths_array[i][4].text)
+
+#for i in range(0,212):
+#    print(i+1," Country ",country_array[i]," Total Cases ", totalCases_array[i][0].text," Total Deaths ",totalDeaths_array[i+1][4].text)
+
+mainArray=[]
+for i in range(0,212):
+    tmp_array=[(country_array[i],totalCases_array[i][0].text,totalDeaths_array[i+1][4].text)]
+    mainArray.append(tmp_array)
 
 
-
-
-            
+labels=["Country","Total Cases","Total Deaths"]
+covid=pd.DataFrame.from_records(mainArray)     
+print(covid)       
+covid.to_csv ("covid_date.csv")
 
 
 
